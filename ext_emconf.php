f7fc8106e304ef02989406b6be143c002f0ecb45
Krzysztof Napora <k.napora@drblitz-weblab.com>
2023-06-27 20:52:26 +0200

    Update PHP support

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.0.0-12.9.99',
-            'php' => '7.4.0-8.2.99',
+            'php' => '8.0.0-8.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
