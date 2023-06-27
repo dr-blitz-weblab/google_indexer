abc38749770dc31dfb53fb4507941ff106464200
Krzysztof Napora <k.napora@drblitz-weblab.com>
2023-06-27 20:49:16 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,15 +2,19 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Google Indexer',
-    'description' => 'Google Indexer',
+    'description' => 'Notify google when you create new pages directly from TYPO3. Supported options: Update, remove and check status',
     'category' => 'module',
-    'author' => '',
+    'author' => 'Krzysztof Napora',
+    'author_company' => 'DR BLITZ WEBLAB',
     'author_email' => 'office@drblitz-weblab.com',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
-        'depends' => [],
+        'depends' => [
+            'typo3' => '11.0.0-11.9.99',
+            'php' => '7.4.0-8.2.99',
+        ],
         'conflicts' => [],
         'suggests' => [],
     ],
