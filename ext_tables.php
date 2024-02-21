b15534e97c26ff321d230c871bcc8b09ac5b1349
Krzysztof Napora <k.napora@drblitz-weblab.com>
2024-02-21 20:33:50 +0100

    Cleaning custom fields when the page is copied

--- ext_tables.php
+++ ext_tables.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processCmdmapClass'][] = \DrBlitz\GoogleIndexer\Hooks\ProcessCmdmap::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \DrBlitz\GoogleIndexer\Hooks\ProcessCmdmap::class;
